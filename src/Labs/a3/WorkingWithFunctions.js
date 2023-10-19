95d30838821d25e131a4e8834f50062ba142887e
Your NameCamusSisyphus <masonwu624@gmail.com>
2023-10-19 16:09:19 -0400

    JavaScript and React.js Assignment 3

---
+++ src/Labs/a3/WorkingWithFunctions.js
@@ -0,0 +1,16 @@
+import ES5Functions from "./ES5Functions";
+import ArrowFunctions from "./ArrowFunctions";
+
+function WorkingWithFunctions() {
+    return(
+        <div>
+        <h2>WorkingWithFunctions</h2>
+        <ES5Functions/>
+        <ArrowFunctions/>
+     </div>
+
+    )
+
+    
+ };
+  export default WorkingWithFunctions;
